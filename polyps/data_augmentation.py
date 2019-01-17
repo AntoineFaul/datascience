b514134a979268ea22a97efdb9186848d8ad7814
Anthony Genson <anthony.genson@isen.yncrea.fr>
2019-01-17 18:05:57 +0100

    Fixed platform path differences + refactoring

--- polyps/data_augmentation.py
+++ polyps/data_augmentation.py
@@ -1,9 +1,10 @@
 from keras.preprocessing.image import ImageDataGenerator
+import platform
 import os
 import glob
 
+
 def createGenerator():
-    
     return  ImageDataGenerator(
             #rotation_range=45, # Degree range for random rotations
             zoom_range= (0.8,1), # Range for random zoom (<1 : zoom in)
@@ -12,15 +13,15 @@ def createGenerator():
             brightness_range = (0.75,1.25),
             rotation_range = 45,
             horizontal_flip=True,  # randomly flip images
-            vertical_flip=True)  # randomly flip images
+            vertical_flip=True  # randomly flip images
+        )
 
 def data_Generator( multiplier, image_path, newImage_path, target_size):
-
     generator = createGenerator()        
     
     for classe in os.listdir(image_path):
-        
         fileList = os.listdir(newImage_path+"\\"+classe)
+
         for fileName in fileList:
             os.remove(newImage_path+"\\"+classe+"\\"+fileName)
 
@@ -42,21 +43,22 @@ def data_Generator( multiplier, image_path, newImage_path, target_size):
                 break
 
 def dataWithLabel_Generator( multiplier, image_path, newImage_path, classData, classLabel, target_size, seed=1):
-
     generatorData = createGenerator()        
     generatorLabel = createGenerator()        
             
     fileList = os.listdir(newImage_path+"\\"+classData)
+
     for fileName in fileList:
         os.remove(newImage_path+"\\"+classData+"\\"+fileName)
         
     fileList = os.listdir(newImage_path+"\\"+classLabel)
+
     for fileName in fileList:
         os.remove(newImage_path+"\\"+classLabel+"\\"+fileName)
 
     path1 = image_path+"\\"+classData+"\\*.jpg"
     path2 = image_path+"\\"+classLabel+"\\*.jpg"
-    print("Augmentation of classe : " + image_path + "\\" + classData + "\n")
+    print("\nAugmentation of classe : " + image_path + "\\" + classData)
     print("Augmentation of classe : " + image_path + "\\" + classLabel + "\n")
 
     imageGenerator = generatorData.flow_from_directory(
@@ -81,23 +83,23 @@ def dataWithLabel_Generator( multiplier, image_path, newImage_path, classData, c
     trainGenerator = zip(imageGenerator, maskGenerator)
         
     for i,batch in enumerate(trainGenerator):
-        print( i)
         if(i >= multiplier-1):
             break
-        
+
     
 if __name__ == "__main__":
 
     # In the folder input, there is two subfolders, the first with pictures and the second with the masks.
     # Same for output folder
     
-    if sys.platform == 'linux':
-        folderInput = "./origin"
-        folderOutput = "./input"
-    else:
+    if platform.system() == 'Windows':
         folderInput = ".\\origin"
         folderOutput = ".\\input"
-    picture_multiplier = 1 # Output number = ${picture_multiplier} * Input_number
+    else:
+        folderInput = "./origin"
+        folderOutput = "./input"
+
+    picture_multiplier = 4 # Output number = ${picture_multiplier} * Input_number
     classData="data" # name of the subfolder containing the picures
     classLabel="label" # name of the subfolder containing the markers
     
