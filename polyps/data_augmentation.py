8774a64f22000bc98ec0ae96cdee4be3495cf953
Anthony Genson <anthony.genson@isen.yncrea.fr>
2019-01-17 12:28:44 +0100

    Change folder structure

--- polyps/data_augmentation.py
+++ polyps/data_augmentation.py
@@ -92,10 +92,10 @@ if __name__ == "__main__":
     # Same for output folder
     
     if sys.platform == 'linux':
-        folderInput = "./"
+        folderInput = "./original"
         folderOutput = "./input"
     else:
-        folderInput = ".\\"
+        folderInput = ".\\original"
         folderOutput = ".\\input"
     picture_multiplier = 1 # Output number = ${picture_multiplier} * Input_number
     classData="data" # name of the subfolder containing the picures
