2dd37dadaab3f1cfaf0305eb5ec81b7db8deb085
Max Schemmer <max.schemmer@gmx.de>
2019-01-19 17:50:21 +0100

    added a segmentation seperate model script

--- Segmentation/model_v3_segmentation.py
+++ Segmentation/model_v3_segmentation.py
@@ -1,19 +1,10 @@
 import u_net_segmentation as us
-from keras.models import Model
-from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D
-from keras.layers.normalization import BatchNormalization
-from keras.layers.core import SpatialDropout2D, Activation
 from keras import backend as K
 from keras.optimizers import Adam
 import numpy as np
 import glob
 from PIL import Image
 import matplotlib.pyplot as plt
-from keras.layers.merge import concatenate
-from keras.utils.data_utils import get_file
-
-# Number of image channels (for example 3 in case of RGB, or 1 for grayscale images)
-CHANNELS = 3
 
 
 def dice_coef(y_true, y_pred):
@@ -37,67 +28,6 @@ def jacard_coef_loss(y_true, y_pred):
 def dice_coef_loss(y_true, y_pred):
     return -dice_coef(y_true, y_pred)
 
-
-def double_conv_layer(x, size, dropout=0.1, batch_norm=True):
-    if K.image_dim_ordering() == 'th':
-        axis = 1
-    else:
-        axis = 3
-    conv = Conv2D(size, (3, 3), padding='same')(x)
-    if batch_norm is True:
-        conv = BatchNormalization(axis=axis)(conv)
-    conv = Activation('relu')(conv)
-    conv = Conv2D(size, (3, 3), padding='same')(conv)
-    if batch_norm is True:
-        conv = BatchNormalization(axis=axis)(conv)
-    conv = Activation('relu')(conv)
-    if dropout > 0:
-        conv = SpatialDropout2D(dropout)(conv)
-    return conv
-
-
-def u_net(dropout_val=0.2, weights=None):
-    inputs = Input((224, 224, CHANNELS))
-    filters = 32
-
-    conv_224 = double_conv_layer(inputs, filters)
-    pool_112 = MaxPooling2D(pool_size=(2, 2))(conv_224)
-
-    conv_112 = double_conv_layer(pool_112, 2*filters)
-    pool_56 = MaxPooling2D(pool_size=(2, 2))(conv_112)
-
-    conv_56 = double_conv_layer(pool_56, 4*filters)
-    pool_28 = MaxPooling2D(pool_size=(2, 2))(conv_56)
-
-    conv_28 = double_conv_layer(pool_28, 8*filters)
-    pool_14 = MaxPooling2D(pool_size=(2, 2))(conv_28)
-
-    conv_14 = double_conv_layer(pool_14, 16*filters)
-    pool_7 = MaxPooling2D(pool_size=(2, 2))(conv_14)
-
-    conv_7 = double_conv_layer(pool_7, 32*filters)
-
-    up_14 = concatenate([UpSampling2D(size=(2, 2))(conv_7), conv_14])
-    up_conv_14 = double_conv_layer(up_14, 16*filters)
-
-    up_28 = concatenate([UpSampling2D(size=(2, 2))(up_conv_14), conv_28])
-    up_conv_28 = double_conv_layer(up_28, 8*filters)
-
-    up_56 = concatenate([UpSampling2D(size=(2, 2))(up_conv_28), conv_56])
-    up_conv_56 = double_conv_layer(up_56, 4*filters)
-
-    up_112 = concatenate([UpSampling2D(size=(2, 2))(up_conv_56), conv_112])
-    up_conv_112 = double_conv_layer(up_112, 2*filters)
-
-    up_224 = concatenate([UpSampling2D(size=(2, 2))(up_conv_112), conv_224])
-    up_conv_224 = double_conv_layer(up_224, filters, dropout_val)
-
-    conv_final = Conv2D(CHANNELS, (1, 1))(up_conv_224)
-    conv_final = Activation('sigmoid')(conv_final)
-
-    model = Model(inputs, conv_final, name="ZF_UNET_224")
-    return model
-
 def load_transform_pictures(folder):
     maxsize = (224,224)
     x_train=[]
@@ -110,15 +40,15 @@ def load_transform_pictures(folder):
     return(x_train)
     
 if __name__ == '__main__':
-    batch_size = 128
-#    model = u_net() #what does the Adam optimizer do
-    model = us.u_net_segmentation()
-    model.compile(optimizer = Adam(lr = 1e-4), loss =  dice_coef_loss , metrics = ['accuracy'])# [dice_coef, 'binary_accuracy', 'mse'])dice_coef_loss
+    batch_size = 64
+    model = us.u_net_segmentation(chanels =3)
+    model.compile(optimizer = Adam(lr = 1e-4), loss =  dice_coef_loss , metrics = ['accuracy',dice_coef,jacard_coef])
     im = np.array(load_transform_pictures('C:\\Users\\MaxSchemmer\\Documents\\git\\datascience_v3\\polyps\\input\\data\\*.jpg'))
     mask = np.array(load_transform_pictures('C:\\Users\\MaxSchemmer\\Documents\\git\\datascience_v3\\polyps\\input\\label\\*.jpg'))
     model.fit(x = im,y=mask,
                         batch_size = batch_size,
-                        epochs = 1, 
+                        epochs = 30, 
+                        validation_split= 0.2
             
                         )
     
--- Segmentation/u_net_segmentation.py
+++ Segmentation/u_net_segmentation.py
@@ -11,66 +11,66 @@ def u_net_segmentation(chanels =3):
     inputs = Input((224, 224, chanels))
     neurons = 64
     
-    c2 = Conv2D(neurons, (3, 3), activation='relu', padding='same') (inputs)
+    c1 = Conv2D(neurons, (3, 3), activation='relu', padding='same') (inputs)
+    c1 = BatchNormalization()(c1)
+    c1 = Conv2D(neurons, (3, 3), activation='relu', padding='same') (c1)       
+    p2 = MaxPooling2D(pool_size=(2, 2))(c1)
+
+    c2 = Conv2D(2*neurons, (3, 3), activation='relu', padding='same') (p2)
     c2 = BatchNormalization()(c2)
-    c2 = Conv2D(neurons, (3, 3), activation='relu', padding='same') (c2)       
-    p2 = MaxPooling2D(pool_size=(2, 2))(c2)
+    c2 = Conv2D(2*neurons, (3, 3), activation='relu', padding='same') (c2)       
+    p3 = MaxPooling2D(pool_size=(2, 2))(c2)
 
-    c3 = Conv2D(2*neurons, (3, 3), activation='relu', padding='same') (p2)
+    c3 = Conv2D(4*neurons, (3, 3), activation='relu', padding='same') (p3)
     c3 = BatchNormalization()(c3)
-    c3 = Conv2D(2*neurons, (3, 3), activation='relu', padding='same') (c3)       
-    p3 = MaxPooling2D(pool_size=(2, 2))(c3)
-
-    c4 = Conv2D(4*neurons, (3, 3), activation='relu', padding='same') (p3)
+    c3 = Conv2D(4*neurons, (3, 3), activation='relu', padding='same') (c3)       
+    p4 = MaxPooling2D(pool_size=(2, 2))(c3)
+    
+    c4 = Conv2D(8*neurons, (3, 3), activation='relu', padding='same') (p4)
     c4 = BatchNormalization()(c4)
-    c4 = Conv2D(4*neurons, (3, 3), activation='relu', padding='same') (c4)       
-    p4 = MaxPooling2D(pool_size=(2, 2))(c4)
+    c4 = Conv2D(8*neurons, (3, 3), activation='relu', padding='same') (c4)       
+    p5 = MaxPooling2D(pool_size=(2, 2))(c4)
     
-    c5 = Conv2D(8*neurons, (3, 3), activation='relu', padding='same') (p4)
+    c5 = Conv2D(16*neurons, (3, 3), activation='relu', padding='same') (p5)
     c5 = BatchNormalization()(c5)
-    c5 = Conv2D(8*neurons, (3, 3), activation='relu', padding='same') (c5)       
-    p5 = MaxPooling2D(pool_size=(2, 2))(c5)
-    
-    c6 = Conv2D(16*neurons, (3, 3), activation='relu', padding='same') (p5)
+    c5 = Conv2D(16*neurons, (3, 3), activation='relu', padding='same') (c5)       
+    p6 = MaxPooling2D(pool_size=(2, 2))(c5)    
+
+
+    c6 = Conv2D(32*neurons, (3, 3), activation='relu', padding='same') (p6)
     c6 = BatchNormalization()(c6)
-    c6 = Conv2D(16*neurons, (3, 3), activation='relu', padding='same') (c6)       
-    p6 = MaxPooling2D(pool_size=(2, 2))(c6)    
+    c6 = Conv2D(32*neurons, (3, 3), activation='relu', padding='same') (c6) 
 
-
-    c7 = Conv2D(32*neurons, (3, 3), activation='relu', padding='same') (p6)
-    c7 = BatchNormalization()(c7)
-    c7 = Conv2D(32*neurons, (3, 3), activation='relu', padding='same') (c7) 
-
-    up1 = concatenate([UpSampling2D(size=(2, 2))(c7), c6])
+    up1 = concatenate([UpSampling2D(size=(2, 2))(c6), c5])
     up_c1 = Conv2D(16*neurons, (3, 3), activation='relu', padding='same') (up1)
     up_c1 = BatchNormalization()(up_c1)
     up_c1 = Conv2D(16*neurons, (3, 3), activation='relu', padding='same') (up_c1) 
 
-    up2 = concatenate([UpSampling2D(size=(2, 2))(up_c1), c5])
-    up_c2 = Conv2D(8*neurons, (3, 3), activation='relu', padding='same') (up2)
+    up2 = concatenate([UpSampling2D(size=(2, 2))(up_c1), c4])
+    up_c1 = Conv2D(8*neurons, (3, 3), activation='relu', padding='same') (up2)
+    up_c1 = BatchNormalization()(up_c1)
+    up_c1 = Conv2D(8*neurons, (3, 3), activation='relu', padding='same') (up_c1)
+
+    up3 = concatenate([UpSampling2D(size=(2, 2))(up_c1), c3])
+    up_c2 = Conv2D(4*neurons, (3, 3), activation='relu', padding='same') (up3)
     up_c2 = BatchNormalization()(up_c2)
-    up_c2 = Conv2D(8*neurons, (3, 3), activation='relu', padding='same') (up_c2)
+    up_c2 = Conv2D(4*neurons, (3, 3), activation='relu', padding='same') (up_c2)
 
-    up3 = concatenate([UpSampling2D(size=(2, 2))(up_c2), c4])
-    up_c3 = Conv2D(4*neurons, (3, 3), activation='relu', padding='same') (up3)
+    up4 = concatenate([UpSampling2D(size=(2, 2))(up_c2), c2])
+    up_c3 = Conv2D(2*neurons, (3, 3), activation='relu', padding='same') (up4)
     up_c3 = BatchNormalization()(up_c3)
-    up_c3 = Conv2D(4*neurons, (3, 3), activation='relu', padding='same') (up_c3)
+    up_c3 = Conv2D(2*neurons, (3, 3), activation='relu', padding='same') (up_c3)
 
-    up4 = concatenate([UpSampling2D(size=(2, 2))(up_c3), c3])
-    up_c4 = Conv2D(2*neurons, (3, 3), activation='relu', padding='same') (up4)
+    up5 = concatenate([UpSampling2D(size=(2, 2))(up_c3), c1])
+    up_c4 = Conv2D(neurons, (3, 3), activation='relu', padding='same') (up5)
     up_c4 = BatchNormalization()(up_c4)
-    up_c4 = Conv2D(2*neurons, (3, 3), activation='relu', padding='same') (up_c4)
-
-    up5 = concatenate([UpSampling2D(size=(2, 2))(up_c4), c2])
-    up_c5 = Conv2D(2*neurons, (3, 3), activation='relu', padding='same') (up5)
-    up_c5 = BatchNormalization()(up_c5)
-    up_c5 = Conv2D(2*neurons, (3, 3), activation='relu', padding='same') (up_c5)
-    up_c5 = Dropout(0.2)(up_c5)
+    up_c4 = Conv2D(neurons, (3, 3), activation='relu', padding='same') (up_c4)
+    up_c4 = Dropout(0.2)(up_c4)
     
-    conv_final = Conv2D(chanels, (1, 1))(up_c5)
+    conv_final = Conv2D(chanels, (1, 1))(up_c4)
     conv_final = Activation('sigmoid')(conv_final)
 
-    model = Model(inputs, conv_final, name="ZF_UNET_224")
+    model = Model(inputs, conv_final)
 #    model.summary()
     return model
 
