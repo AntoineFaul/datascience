61d5e0be53d9e906603386181c8f64fc29afce85
Daniel Aagren Madsen <danielaagrenmadsen@outlook.dk>
2019-01-19 11:35:35 +0100

    Init

---
+++ model_v3_segmentation.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 13 14:26:14 2019
+
+@author: MaxSchemmer
+"""
+
+from keras.models import Model
+from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D
+from keras.layers.normalization import BatchNormalization
+from keras.layers.core import SpatialDropout2D, Activation
+from keras import backend as K
+from keras.optimizers import Adam
+import numpy as np
+import glob
+from PIL import Image
+import matplotlib.pyplot as plt
+from keras.layers.merge import concatenate
+from keras.utils.data_utils import get_file
+
+# Number of image channels (for example 3 in case of RGB, or 1 for grayscale images)
+INPUT_CHANNELS = 3
+# Number of output masks (1 in case you predict only one type of objects)
+OUTPUT_MASK_CHANNELS = 3
+
+
+
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2.0 * intersection + 1.0) / (K.sum(y_true_f) + K.sum(y_pred_f) + 1.0)
+
+
+def jacard_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (intersection + 1.0) / (K.sum(y_true_f) + K.sum(y_pred_f) - intersection + 1.0)
+
+
+def jacard_coef_loss(y_true, y_pred):
+    return -jacard_coef(y_true, y_pred)
+
+
+def dice_coef_loss(y_true, y_pred):
+    return -dice_coef(y_true, y_pred)
+
+
+def double_conv_layer(x, size, dropout=0.0, batch_norm=True):
+    if K.image_dim_ordering() == 'th':
+        axis = 1
+    else:
+        axis = 3
+    conv = Conv2D(size, (3, 3), padding='same')(x)
+    if batch_norm is True:
+        conv = BatchNormalization(axis=axis)(conv)
+    conv = Activation('relu')(conv)
+    conv = Conv2D(size, (3, 3), padding='same')(conv)
+    if batch_norm is True:
+        conv = BatchNormalization(axis=axis)(conv)
+    conv = Activation('relu')(conv)
+    if dropout > 0:
+        conv = SpatialDropout2D(dropout)(conv)
+    return conv
+
+
+def u_net(dropout_val=0.2, weights=None):
+    inputs = Input((224, 224, INPUT_CHANNELS))
+#    axis = 3
+    filters = 32
+
+    conv_224 = double_conv_layer(inputs, filters)
+    pool_112 = MaxPooling2D(pool_size=(2, 2))(conv_224)
+
+    conv_112 = double_conv_layer(pool_112, 2*filters)
+    pool_56 = MaxPooling2D(pool_size=(2, 2))(conv_112)
+
+    conv_56 = double_conv_layer(pool_56, 4*filters)
+    pool_28 = MaxPooling2D(pool_size=(2, 2))(conv_56)
+
+    conv_28 = double_conv_layer(pool_28, 8*filters)
+    pool_14 = MaxPooling2D(pool_size=(2, 2))(conv_28)
+
+    conv_14 = double_conv_layer(pool_14, 16*filters)
+    pool_7 = MaxPooling2D(pool_size=(2, 2))(conv_14)
+
+    conv_7 = double_conv_layer(pool_7, 32*filters)
+
+    up_14 = concatenate([UpSampling2D(size=(2, 2))(conv_7), conv_14])
+    up_conv_14 = double_conv_layer(up_14, 16*filters)
+
+    up_28 = concatenate([UpSampling2D(size=(2, 2))(up_conv_14), conv_28])
+    up_conv_28 = double_conv_layer(up_28, 8*filters)
+
+    up_56 = concatenate([UpSampling2D(size=(2, 2))(up_conv_28), conv_56])
+    up_conv_56 = double_conv_layer(up_56, 4*filters)
+
+    up_112 = concatenate([UpSampling2D(size=(2, 2))(up_conv_56), conv_112])
+    up_conv_112 = double_conv_layer(up_112, 2*filters)
+
+    up_224 = concatenate([UpSampling2D(size=(2, 2))(up_conv_112), conv_224])
+    up_conv_224 = double_conv_layer(up_224, filters, dropout_val)
+
+    conv_final = Conv2D(OUTPUT_MASK_CHANNELS, (1, 1))(up_conv_224)
+    conv_final = Activation('sigmoid')(conv_final)
+
+    model = Model(inputs, conv_final, name="ZF_UNET_224")
+    return model
+
+def load_transform_pictures(folder):
+    maxsize = (224,224)
+    x_train=[]
+    for filename in glob.glob(folder):
+        im=Image.open(filename)
+        im.thumbnail(maxsize)
+#        #normalisation find better function
+#        image_mean = np.mean(im)
+#        im = im / image_mean
+        im = np.array(im)
+        x_train.append(im)
+    return(x_train)
+    
+if __name__ == '__main__':
+    batch_size = 64
+    model = u_net() #what does the Adam optimizer do
+    model.compile(optimizer = Adam(lr = 1e-4), loss =  dice_coef_loss , metrics = ['accuracy'])# [dice_coef, 'binary_accuracy', 'mse'])dice_coef_loss
+    im = np.array(load_transform_pictures('E:\\Projects\\datascience\\polyps\\singletest\\data\\*.jpg'))
+    mask = np.array(load_transform_pictures('E:\\Projects\\datascience\\polyps\\singletest\\label\\*.jpg'))
+    #mask = to_categorical(mask, num_classes=None)
+    model.fit(x = im,y=mask,
+                        steps_per_epoch = 1048//batch_size,#1048//batch_size,
+                        validation_split = 0.2,
+                        validation_steps = 128//batch_size,
+                        epochs = 10, 
+                        )
+    
+    lab_pred = model.predict(im[:1])
+    plt.imshow(lab_pred[0])
+    plt.show()
